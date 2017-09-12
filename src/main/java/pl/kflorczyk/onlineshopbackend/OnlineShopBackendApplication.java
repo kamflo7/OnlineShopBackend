118c61039cc7bebf8b4f40b78cde968017934ab4
kflorczyk <florczykkamil@gmail.com>
2017-09-12 12:50:31 +0200

    Add project

---
+++ src/main/java/pl/kflorczyk/onlineshopbackend/OnlineShopBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.kflorczyk.onlineshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopBackendApplication.class, args);
+	}
+}
